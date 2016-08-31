fc788a9ae8e1519ed6b9ab09da933f161287659a
EltonLaw <eltonlaw296@gmail.com>
2016-08-30 21:14:15 -0400

    Added backpropogation - doesn't learn weights yet
    Feedsforward and then calculates the difference between actual and predicted using quadratic cost function. Then it assigns a portion of the error to each node using the weight vector

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 Basic Machine Learning Algorithms
 =================================
 
-References:
+About
+---------
+Basic implementations of machine learning algorithms. The dataset used is the original MNIST data prepared in sklearn.
+
+
+Notes
 -----------
 
 ### Feed Forward Neural Net
+
+References:
 * http://www.deeplearningbook.org/contents/mlp.html
 * http://www.webpages.ttu.edu/dleverin/neural_network/neural_networks.html
 * http://deeplearning.stanford.edu/wiki/index.php/UFLDL_Tutorial
--- feedforward_nn.py
+++ feedforward_nn.py
@@ -1,66 +1,111 @@
+""" Feedforward Backpropogation Algorithm """
 import numpy as np
-import random
 
-from sklearn.datasets import fetch_mldata
-from sklearn.cross_validation import train_test_split
+# Error Settings
+np.seterr(over="ignore") # To ignore the overflow error from calculating the sigmoid
 
 ### DATA PROCESSING ###
 
-mnist = fetch_mldata('MNIST original')
+# from sklearn.datasets import fetch_mldata
+# from sklearn.cross_validation import train_test_split
+# mnist = fetch_mldata('MNIST original') # Labeled Handwritten digits 0-9
+# x_train,x_test,y_train,y_test = train_test_split(mnist.data,mnist.target,test_size=0.0001,random_state=1)
 
+#################
+#sample data
 
-x_train,x_test,y_train,y_test = train_test_split(mnist.data,mnist.target,test_size=0.3,random_state=1)
+layers = [2,3,1]
+num_samples = 1
+np.random.seed(seed=1)
+data = np.random.randn(num_samples,layers[0])
+labels = np.random.randn(num_samples,layers[-1])
+x_train = data
+x_test=data
+y_train = labels
+y_test = labels
 
-print x_train.shape,x_test.shape,y_train.shape,y_test.shape
 
 ### NEURAL NET ###
 
 # Parameters
-"""
-layers = [8,5,9,4]
-sample_input = np.random.randn(layers[0])
-
-depth = len(layers) # Depth = # of layers
-layers = layers
+batch_size = len(x_train) # 1 = Stochastic GD, 1<max = mini batch, max = batch
+# layers = [len(x_train[0]),400,150,9] # 9 Output layers cause we are looking for the probability of it being each value
+depth = len(layers) 
 biases = [np.random.randn(layer) for layer in layers[1:]] # We don't set biases for neurons in the input layer because biases are only ever used in computing the outputs from later layers
+weights = [np.random.randn(output_layer,input_layer) for input_layer,output_layer in zip(layers[:-1],layers[1:])] # Returns random weights for the all the layers except the last one
+alpha = 0.3 # Learning rate for gradient descent, controls the size of the step
+# trained_weights = []
 
-
-weights = [np.random.randn(input_layer,output_layer) for input_layer,output_layer in zip(layers[:-1],layers[1:])] # Returns random weights for the all the layers except the last one
-
-# Function to produce an S-shaped curve
+# Helper Functions
 def sigmoid(x):
 	return 1.0/(1+np.exp(-x))
 def sigmoid_derivative(x):
 	return sigmoid(x)*(1-sigmoid(x))
+def quadratic_cost(actual,predicted): # For one training example
+	return ((actual-predicted)**2)/float(2)
+def total_quadratic_cost(actual,predicted): # For all training examples
+	return (np.subtract(actual,predicted)**2)/float(2*len(x_train))
+def quadratic_cost_derivative(actual,predicted):
+	return (actual-predicted)
 
-# 
+# used for evaluation
 def forward_propogation(x):
-	A = x
-	i = 0
-	for W,B in zip(weights,biases):
+	"""For each layer we do the dot product of the input vector and the weight vector then add the bias. The result of that is inputted into a sigmoid function. Returns a list containing the output from the final layer """
+	A = x # x is the input layer for layer 1
+	for W,B in zip(weights,biases): # W = weights from layer l, B = bias from layer l+1
+		S = np.add(np.dot(A,np.transpose(W)),B)
+		A = sigmoid(S)
+	return A # This is a list which contains ten numbers representing the probabilities of each label [Pr(0),Pr(1),Pr(2)...Pr(8),Pr(9)]
 
-		print "LAYER:",i
-		print "A:",A
-		print "W",W
-		print "B",B
-		print "Z:",np.dot(A,W)
-		s = np.add(np.dot(A,W),np.transpose(B)) # B is in shape (3,1), and the dot product of A,W is in shape (1,3)
-		print "s:",s
-		A = sigmoid(s)
-		print "A:",A
-		i+=1
-	return A
+def gradient_descent():
+		temp_weights = []
+		for i in reversed(range(len(layers)-1)):
+			for j in range(layers[i]):
+				print ""
+				print "Layer:",i
+				print "Layers[i]/Node:",j
+				for weight in weights[i][j]: # For each weight in layer i,node j. Should iterate through every input nodes weights
+					weight = weight - alpha*cost_derivative()
+					# temp_weights.append(weight)
+		# weights[i][j] = temp_weights # Simultaneously update all weights 
+	
+
+def backward_propogation(x,y): 
+	# Initialize matrix of z's and matrix of activations
+	zs = [np.zeros(layer) for layer in layers]
+	activations = [np.zeros(layer) for layer in layers]
+	
+	zs[0] = x # First z's is the input
+	activations[0]=sigmoid(x)
+
+	a = sigmoid(x) # First activation is the input...Do you sigmoid the first activation?
+	# Feed Forward, calculates z and activation at every node
+	for w,b,i in zip(weights,biases,range(1,len(layers))): 
+		z = np. add(np.dot(a,np.transpose(w)),b)
+		zs[i]=z
+		a = sigmoid(z)
+		activations[i]=a
+	# Backward propogate, calcuates error at output node and uses the weights to calculate error at every other node
+	for layer in reversed(range(len(layers))): #for (n,n-1...2,1)
+		if layer == len(layers)-1:
+			errors = [np.zeros(layer) for layer in layers]
+			errors[-1] = quadratic_cost_derivative(activations[-1],y)*sigmoid_derivative(z[-1])
+		else:
+			# print "SHAPES:",weights[layer-1].shape,errors[layer].shape,sigmoid_derivative(zs[layer]).shape
+			errors[layer] =np.dot(errors[layer+1],weights[layer])*sigmoid_derivative(zs[layer]) # hadamard product
+	print "errors:",errors
+
 
 if __name__ == "__main__":
-	# Debug
-	print "depth:",depth
-	print "layers:",layers
-	print "biases:",biases
-	print "biases_shape:",np.shape(biases)
-	print "weights:",weights
-	print "weights_shape:",np.shape(weights[0])
+	# Uses test data
+	# i = 1
+	# for x,y in zip(x_test,y_test):
+	# 	print "TRIAL:",i
+	# 	predictions = forward_propogation(x)
+	# 	a = forward_propogation
+	# 	i+=1
+	# 	print "Prediction:",float(np.argmax(a))
+	# 	print "Actual:",y 
+	# 	print ""
+	backward_propogation(x_train,y_train)
 
-	print "weights[0][0][1]:",weights[0][0][1]
-	print ""
-	forward_propogation(sample_input) 
-"""
