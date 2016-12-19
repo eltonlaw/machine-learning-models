6eab21e468f8287a7df8b5b57a813bbf8130d889
EltonLaw <eltonlaw296@gmail.com>
2016-12-19 18:21:25 -0500

    Added pickle read/write

--- feedforward_nn/feedforward_v2.py
+++ feedforward_nn/feedforward_v2.py
@@ -1,5 +1,6 @@
 import numpy as np
 np.random.seed(0)
+import pickle
 
 # Get Data
 from sklearn.datasets import fetch_mldata
@@ -9,16 +10,16 @@ mnist = fetch_mldata("MNIST original")
 distinct_outputs = 10
 temp_labels = np.zeros((70000,distinct_outputs))
 for i,label in enumerate(mnist.target):
-	temp_labels[i][label] = 1
+        temp_labels[i][label] = 1
 labels = temp_labels
 del temp_labels
 
+# Split data into train/test set
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(mnist.data,labels,test_size=0.3,random_state=1)
-del labels
-del mnist
+del labels,mnist
 
-# Layer
+# Layers Array
 input_layer = len(X_train[0])
 output_layer = distinct_outputs
 layers = [input_layer,3,output_layer] # [784,3,10]
@@ -30,23 +31,28 @@ weights = [np.random.uniform(low=-1.0,high=1.0,size=(nodes)) for nodes in init_w
 
 # TRAINING
 for data,label in zip(X_train,y_train):
-	Z = [np.empty(layer) for layer in layers]
+        Z = [np.empty(layer) for layer in layers]
         # Z[0]:(784,) Z[1]:(3,), Z[2]:(10,)
-        
 
+# TESTING
+correct = 0
+total = len(y_test)
+for data,label in zip(X_test,y_test):
+    prediction = forward_propogation(data)
+    if prediction == label:
+        correct += 1.0
+accuracy_score = correct/total
+print(accuracy_score)
 
-for _ in Z:
-    print np.shape(_)
-
-	
 def forward_propogation(x):
     """ Feed an unknown x through the neural net and return a classification label"""
-    for W_i,B_i in zip(Weights,Bias):
+    for W_i,B_i in zip(Weights,Biases):
         z = np.matmul(W_i,x)+B_i
         a = ReLU(z)
         x = a # Sets next layer's input as this layer's activation
     output = softmax(z) # Instead of using ReLU for the last layer, use softmax
     return output
+
 
 # Activation Functions
 def ReLU(a):
@@ -61,4 +67,16 @@ def softmax_delta(a):
 # Cost Function
 def cross_entropy(a,b): 
 
-
+### HELPER FUNCTIONS
+# Save and read trained model parameters
+def save(weights,biases,path):
+    """ Save weights and biases into a pickle"""
+    data = {"weights":weights,
+            "biases":biases}
+    with open("./output/data.pkl","wb") as o:
+        pickle.dump(data,o)
+def read(pickle_location):
+    """_pickle_location: String path of pickle file """
+    with open(pickle_location,"rb") as p:
+        data = pickle.load(p)
+    return data
